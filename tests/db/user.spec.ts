04628e68c88090d0c6be65ed47f9010fb681ffdb
Robin <rteuwens9206@gmail.com>
2023-01-18 21:46:22 +0100

    Create user in beforeAll(). Added bcrypt.compare test.

--- tests/db/user.spec.ts
+++ tests/db/user.spec.ts
@@ -1,47 +1,49 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import User from '../../src/db/models/user';
 import * as path from 'path';
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
 
 describe('User Model', () => {
-    // Connect to the test database before running the tests
+
+    const userData = {
+        email: 'test@example.com',
+        password: 'testpassword',
+    };
+
     beforeAll(async () => {
         mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.MONGO_URL);
+        const user = new User(userData);
+        await user.save();
     });
-
-    // Clear all test data after running the tests
-    afterEach(async () => {
-        await User.deleteMany({});
-    });
-
-    // Close the connection to the test database after all tests have run
+    
     afterAll(async () => {
+        await User.deleteMany({});
         await mongoose.connection.dropDatabase()
         await mongoose.connection.close();
     });
 
-    it('should create a new user', async () => {
-        const userData = {
-            email: 'test@example.com',
-            password: 'testpassword',
-        };
-        const user = new User(userData);
-        await user.save();
-        const users = await User.find({ email: 'test@example.com' });
-        expect(users.length).toBe(1);
-        expect(users[0].email).toBe(userData.email);
+    it('should have created a new user', async () => {
+        const user = await User.findOne({ email: 'test@example.com' });
+        expect(user).toBeInstanceOf(User);
+        expect(user.email).toBe(userData.email);
     });
 
-    it('should hash the password before saving the user', async () => {
-        const userData = {
-            email: 'test@example.com',
-            password: 'testpassword',
-        };
-        const user = new User(userData);
-        await user.save();
-        const users = await User.find({ email: 'test@example.com' });
-        expect(users[0].password).not.toBe(userData.password);
+    it('should have a hashed password', async () => {
+        const user = await User.findOne({ email: userData.email });
+        expect(user.password).not.toBe(userData.password);
+    });
+
+    it('should have a createdAt field with a valid date', async () => {
+        const user = await User.findOne({ email: userData.email });
+        expect(user.createdAt).toBeInstanceOf(Date);
+    });
+
+    it('should correctly match password to log in', async () => {
+        const user = await User.findOne({ email: userData.email });
+        const match = await bcrypt.compare(userData.password, user.password)
+        expect(match).toBeTruthy
     });
 });
